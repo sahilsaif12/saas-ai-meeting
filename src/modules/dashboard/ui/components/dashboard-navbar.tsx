75999feb0c875cd537e8d39a6354e511bbd0ed20
sahil_saif <sahilsaif2002@gmail.com>
2025-07-10 20:08:36 +0530

    added globar search with command

--- src/modules/dashboard/ui/components/dashboard-navbar.tsx
+++ src/modules/dashboard/ui/components/dashboard-navbar.tsx
@@ -10,6 +10,18 @@ export const DashboardNavbar = () => {
     const { state, toggleSidebar, isMobile } = useSidebar()
     const [commandOpen, setCommandOpen] = useState(false)
 
+     useEffect(() => {
+        const down=(e: KeyboardEvent)=>{
+            if (e.key==="k" &&( e.metaKey  || e.ctrlKey) ) {
+                e.preventDefault()
+                setCommandOpen((open)=>!open)
+            }
+        }
+
+        document.addEventListener("keydown",down)
+
+        return ()=> document.removeEventListener("keydown",down)
+    }, [])
     
     return (
         <>
---
+++ src/modules/dashboard/ui/components/dashboard-search-command.tsx
@@ -0,0 +1,29 @@
+"use client"
+import { CommandDialog, CommandInput, CommandList } from "@/components/ui/command"
+import { CommandItem } from "cmdk";
+import { Dispatch, SetStateAction } from "react";
+
+interface Props{
+    open:boolean;
+    setOpen:Dispatch<SetStateAction<boolean>>
+}
+
+export const DashboardSearchCommand =({open,setOpen}:Props)=>{
+    return(
+        <CommandDialog open={open} onOpenChange={setOpen} >
+            <CommandInput
+            placeholder="Find a meeting or agent"
+           />
+                <CommandList>
+
+                    <CommandItem>
+                        Testing
+                    </CommandItem>
+                </CommandList>
+
+
+
+        </CommandDialog>
+
+    )
+}
